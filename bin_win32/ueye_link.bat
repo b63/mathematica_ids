42160cff9d0460ce6d097db2aa17969fe83c247d
unknown <b63>
2021-11-13 12:41:06 -0500

    proper release qt dlls

--- bin_win32/ueye_link.bat
+++ bin_win32/ueye_link.bat
@@ -1,3 +1,3 @@
 @echo off
 SET "PATH=C:\Program Files\IDS\ids_peak\sdk\api\lib\x86_64;C:\Program Files\IDS\ids_peak\sdk\ipl\lib\x86_64;C:\Program Files\Wolfram Research\Mathematica\12.3\SystemFiles\Links\WSTP\DeveloperKit\Windows-x86-64\SystemAdditions; %PATH%"
-.\Debug\ueye_link.exe
+.\Release\ueye_link.exe
